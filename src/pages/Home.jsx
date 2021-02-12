f417674a59133f3f11f3dd72849ee6feca20252e
matthewghuang <matthewhhuang@gmail.com>
2021-02-11 18:01:27 -0800

    class to className

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,9 @@ import "../styles/link.css"
 export const Home = () => {
 	return (
 		<div className="container mx-auto">
-			<h1 class="text-lg">Probability Visualizer</h1>
+			<h1 className="text-2xl">Probability Visualizer</h1>
 
-			<Link to="/coin-flip" class="link">
+			<Link to="/coin-flip" className="link">
 				Coin Flip
 			</Link>
 		</div>
