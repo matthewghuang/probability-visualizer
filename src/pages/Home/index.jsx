d07dc5bcff39b2c8ba49f14a8d8701b76e96e118
matthewghuang <matthewhhuang@gmail.com>
2021-02-12 15:50:36 -0800

    Home: add coins section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,14 +6,17 @@ export const Home = () => {
 		<div className="container mx-auto">
 			<h1 className="text-2xl">Probability Visualizer</h1>
 
-			<ul>
-				<li>
-					<BlueLink to="/coin-flip">Coin Flip</BlueLink>
-				</li>
-				<li>
-					<BlueLink to="/coin-guesser">Coin Guesser</BlueLink>
-				</li>
-			</ul>
+			<div>
+				<span className="text-lg font-bold">Coins</span>
+				<ul>
+					<li>
+						<BlueLink to="/coin-flip">Coin Flip</BlueLink>
+					</li>
+					<li>
+						<BlueLink to="/coin-guesser">Coin Guesser</BlueLink>
+					</li>
+				</ul>
+			</div>
 		</div>
 	)
 }
