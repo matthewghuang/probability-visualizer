46c979652d77dafff4b60a7e7bac3f37784d45f3
matthewghuang <matthewhhuang@gmail.com>
2021-02-12 15:19:13 -0800

    CoinGuesser: add trials, style button

--- src/pages/CoinGuesser/index.jsx
+++ src/pages/CoinGuesser/index.jsx
@@ -6,14 +6,18 @@ const Coin = styled.div.attrs({
 	className:
 		"flex bg-yellow-800 w-24 h-24 rounded-full justify-center items-center text-3xl text-white select-none"
 })``
-
-const theoretical_probability = n => {
-	return (1 * 2) / 2 ** n
-}
+const Button = styled.button.attrs({
+	className: "p-1 rounded-lg bg-blue-400"
+})``
 
 export const CoinGuesser = () => {
-	const [coins, setCoins] = useState([])
+	const [coins, _setCoins] = useState([])
 	const [trials, setTrials] = useState([])
+
+	const setCoins = arr => {
+		setTrials([])
+		_setCoins(arr)
+	}
 
 	const on_number_of_coins_input = event =>
 		setCoins(Array(Number(event.target.value)).fill("?"))
@@ -39,6 +43,36 @@ export const CoinGuesser = () => {
 		])
 	}
 
+	const generate_random_coin = () => (Math.round(Math.random()) ? "H" : "T")
+
+	const add_trial = () => {
+		const trial = [...Array(coins.length)].map(generate_random_coin)
+
+		setTrials([...trials, trial])
+	}
+
+	const add_hundred_trials = () => {
+		const new_trials = [...Array(100)].map(() =>
+			[...Array(coins.length)].map(generate_random_coin)
+		)
+		setTrials([...trials, ...new_trials])
+	}
+	const theoretical_probability = () =>
+		(1 / 2) ** coins.filter(c => c == "H" || C == "T").length
+
+	const experimental_probability = () => {
+		const matches = trials.map(trial => {
+			return trial.every((t, i) => {
+				const c = coins[i]
+
+				if (c == "?") return true
+				else return t == c
+			})
+		})
+
+		return matches.filter(m => m == true).length / trials.length
+	}
+
 	return (
 		<div className="container mx-auto space-y-3">
 			<div>
@@ -46,16 +80,23 @@ export const CoinGuesser = () => {
 				<BlueLink to="/">Home</BlueLink>
 			</div>
 
-			<div>
-				<label htmlFor="number-of-coins">Number of Coins: </label>
-				<input
-					type="number"
-					name="number-of-coins"
-					onInput={on_number_of_coins_input}
-					min="0"
-					max="10"
-					className="p-1 bg-blue-400 rounded-lg"
-				/>
+			<div className="space-y-1">
+				<fieldset>
+					<label htmlFor="number-of-coins">Number of Coins: </label>
+					<input
+						type="number"
+						name="number-of-coins"
+						onInput={on_number_of_coins_input}
+						min="0"
+						max="10"
+						className="p-1 bg-blue-400 rounded-lg"
+					/>
+				</fieldset>
+				<fieldset className="space-x-1">
+					<Button onClick={add_trial}>Test</Button>
+					<Button onClick={add_hundred_trials}>Test 100</Button>
+					<Button onClick={() => setTrials([])}>Reset Tests</Button>
+				</fieldset>
 			</div>
 
 			<div className="flex space-x-1">
@@ -68,12 +109,12 @@ export const CoinGuesser = () => {
 
 			<div>
 				Theoretical Probability:{" "}
-				<span className="font-mono">
-					{theoretical_probability(coins.length) * 100}%
-				</span>
+				<span className="font-mono">{theoretical_probability() * 100}%</span>
 				<br />
 				Experimental Probablity ({trials.length} trials):{" "}
-				<span className="font-mono"></span>
+				<span className="font-mono">
+					{experimental_probability() * 100 || 0}%
+				</span>
 			</div>
 		</div>
 	)
